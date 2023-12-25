f1997c7b9dbd7b6f14a9a88cb956d56fed7460a0
Aadesh Malani <aadeshmalani@gmail.com>
2023-12-25 21:23:11 +0530

    implemented a succes web page for easy navigation and intregated cloud API successfully

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from datetime import datetime
-import os,io
+import os
+import re
 from google.cloud import vision_v1
 from google.cloud.vision_v1 import types
 
@@ -11,15 +13,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:(Aady24)@lo
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class OCRRecord(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     identification_number = db.Column(db.String(20))
-    name = db.Column(db.String(50))
-    last_name = db.Column(db.String(50))
-    date_of_birth = db.Column(db.String(10))
-    date_of_issue = db.Column(db.String(10))
-    date_of_expiry = db.Column(db.String(10))
+    name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    date_of_birth = db.Column(db.String(20))
+    date_of_issue = db.Column(db.String(20))
+    date_of_expiry = db.Column(db.String(20))
     timestamp = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp())
     status = db.Column(db.String(10))
     error_message = db.Column(db.String(255))
@@ -54,12 +57,12 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/upload', methods=['GET','POST'])
 def upload_image():
-    print("hello world",flush=True)
+    # print("hello world",flush=True)
     if 'file' not in request.files:
         return jsonify({"status": "failure", "message": "No file provided"})
     file = request.files['file']
 
-    print(file)
+    # print(file)
     if file.filename == '':
         return jsonify({"status": "failure", "message": "No file selected"})
 
@@ -67,18 +70,66 @@ def upload_image():
         if not os.path.exists(app.config['UPLOAD_FOLDER']):
             os.makedirs(app.config['UPLOAD_FOLDER'])
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        print(file_path)
+        # print(file_path)
         file.save(file_path)
 
         ocr_result = process_image(file_path)
+        # print(ocr_result)
 
         if ocr_result:
-            identification_number = "123456789"
-            name = "John"
-            last_name = "Doe"
-            date_of_birth = "01/01/1990"
-            date_of_issue = "01/01/2022"
-            date_of_expiry = "01/01/2030"
+
+            # Define regular expressions
+            id_number_regex = re.compile(r"(\d\s\d{4}\s\d{5}\s\d{2}\s\d)")
+            name_regex = re.compile(r"Name (.+)")
+            last_name_regex = re.compile(r"Last name (.+)")
+            dob_regex = re.compile(r"Date of Birth (\d{1,2} [A-Za-z]+\. \d{4})")
+            issue_date_regex = re.compile(r"(\d{1,2} [A-Za-z]+\. \d{4})[\n\s]+Date of Issue")
+            expiry_date_regex = re.compile(r"(\d{1,2} [A-Za-z]+\. \d{4})[\n\s]+Date of Expiry")
+
+            # Extract information using regular expressions with null checks
+            id_number_match = id_number_regex.search(ocr_result)
+            name_match = name_regex.search(ocr_result)
+            last_name_match = last_name_regex.search(ocr_result)
+            dob_match = dob_regex.search(ocr_result)
+            issue_date_match = issue_date_regex.search(ocr_result)
+            expiry_date_match = expiry_date_regex.search(ocr_result)
+
+            # Define matches
+            matches = [
+                id_number_match,
+                name_match,
+                last_name_match,
+                dob_match,
+                issue_date_match,
+                expiry_date_match,
+            ]
+
+            # Count the non-null matches
+            non_null_matches = [match for match in matches if match is not None]
+
+            # Calculate the percentage
+            percentage_success = (len(non_null_matches) / len(matches)) * 100
+
+            # Return extracted information
+            OCR_parsed =  {
+                "idNumber": id_number_match.group(1) if id_number_match else "Not found",
+                "name": name_match.group(1) if name_match else "Not found",
+                "lastName": last_name_match.group(1) if last_name_match else "Not found",
+                "dob": dob_match.group(1) if dob_match else "Not found",
+                "issueDate": issue_date_match.group(1) if issue_date_match else "Not found",
+                "expiryDate": expiry_date_match.group(1) if expiry_date_match else "Not found",
+                "percentageSuccess": percentage_success,
+                "rawData": ocr_result
+            }
+            # print(OCR_parsed)
+
+
+            identification_number = OCR_parsed["idNumber"]
+            name = OCR_parsed["name"]
+            last_name = OCR_parsed["lastName"]
+            date_of_birth = OCR_parsed["dob"]
+            date_of_issue = OCR_parsed["issueDate"]
+            date_of_expiry = OCR_parsed["expiryDate"]
 
             new_record = OCRRecord(
                 identification_number=identification_number,
@@ -92,10 +143,16 @@ def upload_image():
 
             db.session.add(new_record)
             db.session.commit()
-
-            return jsonify({"status": "success", "message": "OCR processed successfully"})
+            return render_template('success.html')
+            # return jsonify({"status": "success", "message": "OCR processed successfully"})
         else:
             return jsonify({"status": "failure", "message": "Unable to extract text from the image"})
+
+# Add a new route for the success page
+@app.route('/success', methods=['GET'])
+def success():
+    return render_template('success.html')
+
 
 @app.route('/ocr-data', methods=['GET'])
 def get_ocr_data():
